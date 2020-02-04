764523113c0ce46257a73d1263ed3da36d39e7ba
imyatharth11 <54251839+imyatharth11@users.noreply.github.com>
2020-02-04 23:12:44 +0530

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,43 +1,43 @@
 
 CREATE TABLE professionCategory(
-	pid int,
+    pid int,
     profession VARCHAR(32) DEFAULT 'NONE',
     domain VARCHAR(32) DEFAULT 'NONE',
     PRIMARY KEY(pid)
 );
 
 CREATE TABLE user(
-	uid int AUTO_INCREMENT,
+    uid int AUTO_INCREMENT,
     username VARCHAR(50),
     password VARCHAR(50),
     name VARCHAR(50),
     age int,
-    verficationDoc int,
-    #pid int,
-    PRIMARY KEY(uid)
-    #FOREIGN KEY(pid) REFERENCES ProfessionCategory(pid) ON DELETE SET NULL
+    aadharNumber bigint UNIQUE,
+    pid int,
+    PRIMARY KEY(uid),
+    FOREIGN KEY(pid) REFERENCES ProfessionCategory(pid) ON DELETE SET NULL
 );
 
 CREATE TABLE location(
-	lid int,
-	locality VARCHAR(250),
+    lid int,
+    locality VARCHAR(250),
     pin int,
     city VARCHAR(50),
     PRIMARY KEY(lid)
 );
 
 CREATE TABLE address(
-	uid int,
-    address VARCHAR(250),
+    uid int,
+    appartment VARCHAR(250),
     lid int,
     addressType VARCHAR(50) DEFAULT 'HOME',
-    PRIMARY KEY(uid, address),
+    PRIMARY KEY(uid, appartment),
     FOREIGN KEY(uid) REFERENCES user(uid) ON DELETE CASCADE,
     FOREIGN KEY(lid) REFERENCES location(lid) ON DELETE SET NULL
 );
 
 CREATE TABLE favourites(
-	uidSS int,	-- SS -> Service Seeker
+    uidSS int,	-- SS -> Service Seeker
     uidSP int,	-- SP -> Service Provider
     PRIMARY KEY(uidSS, uidSP),
     FOREIGN KEY(uidSS) REFERENCES user(uid) ON DELETE CASCADE,
@@ -45,31 +45,31 @@ CREATE TABLE favourites(
 );
 
 CREATE TABLE phoneNumber(
-	uid int,
+    uid int,
     phoneNumber int,
     PRIMARY KEY(uid, phoneNumber),
     FOREIGN KEY(uid) REFERENCES user(uid) ON DELETE CASCADE
 );
 
 CREATE TABLE sosNumber(
-	uid int,
+    uid int,
     phoneNumber int,
     PRIMARY KEY(uid, phoneNumber),
     FOREIGN KEY(uid) REFERENCES user(uid) ON DELETE CASCADE
 );
 
 CREATE TABLE ratingsAndReviews(
-	uidSS int, -- SS -> Service Seeker
+    uidSS int, -- SS -> Service Seeker
     uidSP int, -- SP -> Service Provider
-    rating DECIMAL(2,1),
-    review VARCHAR(200),
+    rating int,
+    review VARCHAR(500),
     PRIMARY KEY(uidSS, uidSP),
-	FOREIGN KEY(uidSS) REFERENCES user(uid) ON DELETE CASCADE,
+    FOREIGN KEY(uidSS) REFERENCES user(uid) ON DELETE CASCADE,
     FOREIGN KEY(uidSP) REFERENCES user(uid) ON DELETE CASCADE
 );
 
 CREATE TABLE employee(
-	eid int,
+    eid int,
     name VARCHAR(32),
     designation VARCHAR(32),
     availability VARCHAR(15),
@@ -77,18 +77,19 @@ CREATE TABLE employee(
 );
 
 CREATE TABLE complaints(
-	uidSS int, -- SS -> Service Seeker
+    cid int,
+    uidSS int, -- SS -> Service Seeker
     uidSP int, -- SP -> Service Provider
     complaint VARCHAR(200),
     eid int,
-    PRIMARY KEY(uidSS, uidSP),
-	FOREIGN KEY(uidSS) REFERENCES user(uid) ON DELETE CASCADE,
+    PRIMARY KEY(cid),
+    FOREIGN KEY(uidSS) REFERENCES user(uid) ON DELETE CASCADE,
     FOREIGN KEY(uidSP) REFERENCES user(uid) ON DELETE CASCADE,
     FOREIGN KEY(eid) REFERENCES employee(eid) ON DELETE CASCADE
 );
 
 CREATE TABLE blacklist(
-	phoneNumber int PRIMARY KEY
+	aadharNumber int PRIMARY KEY
 );
 
 show tables;
@@ -106,16 +107,15 @@ DESCRIBE employee
 DESCRIBE complaints
 DESCRIBE blacklist
 
-
 DROP TABLE professionCategory;
-DROP TABLE user CASCADE;
-DROP TABLE location CASCADE;
-DROP TABLE address CASCADE;
-DROP TABLE favourites CASCADE;
-DROP TABLE phoneNumber CASCADE;
-DROP TABLE sosNumber CASCADE;
-DROP TABLE ratingsAndReviews CASCADE;
-DROP TABLE employee CASCADE;
-DROP TABLE complaints CASCADE;
-DROP TABLE blacklist CASCADE;
+DROP TABLE user;
+DROP TABLE location;
+DROP TABLE address;
+DROP TABLE favourites;
+DROP TABLE phoneNumber;
+DROP TABLE sosNumber;
+DROP TABLE ratingsAndReviews;
+DROP TABLE employee;
+DROP TABLE complaints;
+DROP TABLE blacklist;
 */
